54759a440a00d69844e2dcd9a25424744fe656b7
ben-sack <mr.benjaminsack@gmail.com>
2023-02-10 07:45:31 -0800

    update links

--- _next/static/vQRY6FIBFGyZM1Xa0Sj9T/_ssgManifest.js
+++ _next/static/vQRY6FIBFGyZM1Xa0Sj9T/_ssgManifest.js
@@ -1,1 +1,1 @@
-// self.__SSG_MANIFEST=new Set(["\u002Fcraft","\u002Fphotos"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
+self.__SSG_MANIFEST=new Set(["\u002Fcraft","\u002Fphotos"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
